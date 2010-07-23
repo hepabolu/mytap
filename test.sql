2deeb453a51687756e3fe0a348bfac77dec97cd7
David E. Wheeler <david@justatheory.com>
2010-07-23 16:19:41 -0700

    Schema-qualify call to `tap()` in test.

--- test.sql
+++ test.sql
@@ -23,7 +23,7 @@ SELECT tap.isnt_eq('cbc', NULL, 'isnt_eq with null');
 SELECT tap.isnt_eq('cbc', 'bcb', 'isnt_eq with strings');
 SELECT tap.isnt_eq('cbc', 'cbc', 'isnt_eq with matching strings');
 SELECT tap.isnt_eq(NULL, NULL, 'isnt_eq with NULLs');
-select diag('hey\nthere');
+select tap.diag('hey\nthere');
 SELECT tap.ok(maxlen > 1, concat(character_set_name, ' should have length > 1'))
       FROM information_schema.character_sets;
 
