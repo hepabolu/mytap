c18d0ecd667e3d617d4cf375c9b339fa38eb228b
Helma van der Linden <hepabolu@gmail.com>
2014-05-29 11:24:20 +0200

    Functions rewritten to remove descriptions as parameters and functions added to check primary and index keys

--- mytap-column.sql
+++ mytap-column.sql
@@ -21,77 +21,175 @@ END //
 
 -- has_column( schema, table, column, description )
 DROP FUNCTION IF EXISTS has_column //
-CREATE FUNCTION has_column ( dbname TEXT, tname TEXT, cname TEXT, description TEXT )
+CREATE FUNCTION has_column ( dbname TEXT, tname TEXT, cname TEXT )
 RETURNS TEXT
 BEGIN
-	if description = '' then
-		set description = concat('Column ', quote_ident(tname), '.', quote_ident(cname), ' should exist' );
-	end if;
-	
-    return ok( _cexists( dbname, tname, cname ), description );
+    return ok( 
+      _cexists( dbname, tname, cname ),
+      concat('Column ', quote_ident(tname), '.', quote_ident(cname), ' should exist' )
+   );
 END //
 
 
 -- hasnt_column( schema, table, column, description )
 DROP FUNCTION IF EXISTS hasnt_column //
-CREATE FUNCTION hasnt_column ( dbname TEXT, tname TEXT, cname TEXT, description TEXT )
+CREATE FUNCTION hasnt_column ( dbname TEXT, tname TEXT, cname TEXT )
 RETURNS TEXT
 BEGIN
-
-	if description = '' then
-		set description = concat('Column ', quote_ident(tname), '.', quote_ident(cname), ' should not exist' );
-	end if;
-	
-    return ok( NOT _cexists( dbname, tname, cname ), description );
+    return ok( 
+        NOT _cexists( dbname, tname, cname ), 
+        concat('Column ', quote_ident(tname), '.', quote_ident(cname), ' should not exist' )
+    );
 END //
 
 
 /**************************/
--- _col_is_null( schema, table, column, desc, bool )
+-- _col_is_nullable ( schema, table, column, bool )
 DROP FUNCTION IF EXISTS _col_is_nullable //
-CREATE FUNCTION _col_is_nullable ( dbname TEXT, tname TEXT, cname TEXT, description TEXT, cbool varchar(3))
+CREATE FUNCTION _col_is_nullable ( dbname TEXT, tname TEXT, cname TEXT, cbool varchar(3))
+RETURNS BOOLEAN
+BEGIN
+    DECLARE ret BOOLEAN;
+
+    SELECT true into ret
+      FROM information_schema.columns as db
+     WHERE db.table_schema = dbname
+       AND db.table_name = tname
+       AND db.column_name = cname
+       AND db.is_nullable = cbool;
+    RETURN coalesce(ret, false);
+END //
+
+-- col_is_null( schema, table, column )
+DROP FUNCTION IF EXISTS col_is_null //
+CREATE FUNCTION col_is_null ( dbname TEXT, tname TEXT, cname TEXT )
 RETURNS TEXT
 BEGIN
     IF NOT _cexists( dbname, tname, cname ) THEN
-        RETURN concat(fail( description ), 'E\n',
-               diag (concat('    Column ', quote_ident(dbname), '.', quote_ident(tname), '.', quote_ident(cname), ' does not exist' )));
+        RETURN fail(concat('Error ',
+               diag (concat('    Column ', quote_ident(dbname), '.', quote_ident(tname), '.', quote_ident(cname), ' does not exist' ))));
     END IF;
     RETURN ok(
-        EXISTS(
-            SELECT true
-              FROM information_schema.columns as db
-             WHERE db.table_schema = dbname
-               AND db.table_name = tname
-               AND db.column_name = cname
-               AND db.is_nullable = cbool
-        ), description
+        _col_is_nullable(dbname, tname, cname, 'YES'),
+        concat('Column ', quote_ident(tname), '.', quote_ident(cname), ' should allow NULL' )
     );
 END //
 
 -- col_not_null( schema, table, column, description )
 DROP FUNCTION IF EXISTS col_not_null //
-CREATE FUNCTION col_not_null ( dbname TEXT, tname TEXT, cname TEXT, description TEXT )
+CREATE FUNCTION col_not_null ( dbname TEXT, tname TEXT, cname TEXT )
 RETURNS TEXT
 BEGIN
-	if description = '' then
-		set description = concat('Column ', quote_ident(tname), '.', quote_ident(cname), ' should not allow NULL' );
-	end if;
-
-    return _col_is_nullable( dbname, tname, cname, description, 'NO' );
+    IF NOT _cexists( dbname, tname, cname ) THEN
+        RETURN fail(concat('Error ',
+               diag (concat('    Column ', quote_ident(dbname), '.', quote_ident(tname), '.', quote_ident(cname), ' does not exist' ))));
+    END IF;
+    RETURN ok(
+        _col_is_nullable(dbname, tname, cname, 'NO'),
+        concat('Column ', quote_ident(tname), '.', quote_ident(cname), ' should not allow NULL' )
+    );
 END //
 
 
--- col_is_null( schema, table, column, description )
-DROP FUNCTION IF EXISTS col_is_null //
-CREATE FUNCTION col_is_null ( dbname TEXT, tname TEXT, cname TEXT, description TEXT )
+/****************************************************************************/
+
+-- _col_has_primary_key ( schema, table, column )
+
+DROP FUNCTION IF EXISTS _col_has_primary_key //
+CREATE FUNCTION _col_has_primary_key ( dbname TEXT, tname TEXT, cname TEXT )
+RETURNS BOOLEAN
+BEGIN
+    DECLARE ret BOOLEAN;
+
+    SELECT true into ret
+      FROM information_schema.columns as db
+     WHERE db.table_schema = dbname
+       AND db.table_name = tname
+       AND db.column_name = cname
+       AND db.column_key = 'PRI';
+    RETURN coalesce(ret, false);
+END //
+
+-- col_has_primary_key ( schema, table, column )
+DROP FUNCTION IF EXISTS col_has_primary_key //
+CREATE FUNCTION col_has_primary_key ( dbname TEXT, tname TEXT, cname TEXT )
 RETURNS TEXT
 BEGIN
-	if description = '' then
-		set description = concat('Column ', quote_ident(tname), '.', quote_ident(cname), ' should allow NULL' );
-	end if;
-	
-    return _col_is_nullable( dbname, tname, cname, description, 'YES' );
+    IF NOT _cexists( dbname, tname, cname ) THEN
+        RETURN fail(concat('Error ',
+               diag (concat('    Column ', quote_ident(dbname), '.', quote_ident(tname), '.', quote_ident(cname), ' does not exist' ))));
+    END IF;
+    RETURN ok(
+        _col_has_primary_key(dbname, tname, cname),
+        concat('Column ', quote_ident(tname), '.', quote_ident(cname), ' should have PRIMARY key' )
+    );
 END //
+
+-- col_hasnt_primary_key( schema, table, column )
+DROP FUNCTION IF EXISTS col_hasnt_primary_key //
+CREATE FUNCTION col_hasnt_primary_key ( dbname TEXT, tname TEXT, cname TEXT )
+RETURNS TEXT
+BEGIN
+    IF NOT _cexists( dbname, tname, cname ) THEN
+        RETURN fail(concat('Error ',
+               diag (concat('    Column ', quote_ident(dbname), '.', quote_ident(tname), '.', quote_ident(cname), ' does not exist' ))));
+    END IF;
+    RETURN ok(
+        NOT _col_has_primary_key(dbname, tname, cname),
+        concat('Column ', quote_ident(tname), '.', quote_ident(cname), ' should not have PRIMARY key' )
+    );
+END //
+
+/****************************************************************************/
+
+-- _col_has_index_key (schema, table, column )
+
+DROP FUNCTION IF EXISTS _col_has_index_key //
+CREATE FUNCTION _col_has_index_key ( dbname TEXT, tname TEXT, cname TEXT )
+RETURNS BOOLEAN
+BEGIN
+    DECLARE ret BOOLEAN;
+
+    SELECT true into ret
+      FROM information_schema.statistics as db
+     WHERE db.table_schema = dbname
+       AND db.table_name = tname
+       AND db.column_name = cname
+       AND db.index_name <> 'PRIMARY';
+    RETURN coalesce(ret, false);
+END //
+
+-- col_has_index_key ( schema, table, column )
+DROP FUNCTION IF EXISTS col_has_index_key //
+CREATE FUNCTION col_has_index_key ( dbname TEXT, tname TEXT, cname TEXT )
+RETURNS TEXT
+BEGIN
+    IF NOT _cexists( dbname, tname, cname ) THEN
+        RETURN fail(concat('Error ',
+               diag (concat('    Column ', quote_ident(dbname), '.', quote_ident(tname), '.', quote_ident(cname), ' does not exist' ))));
+    END IF;
+    RETURN ok(
+        _col_has_index_key(dbname, tname, cname),
+        concat('Column ', quote_ident(tname), '.', quote_ident(cname), ' should have INDEX key' )
+    );
+END //
+
+-- col_hasnt_index_key( schema, table, column )
+DROP FUNCTION IF EXISTS col_hasnt_index_key //
+CREATE FUNCTION col_hasnt_index_key ( dbname TEXT, tname TEXT, cname TEXT )
+RETURNS TEXT
+BEGIN
+    IF NOT _cexists( dbname, tname, cname ) THEN
+        RETURN fail(concat('Error ',
+               diag (concat('    Column ', quote_ident(dbname), '.', quote_ident(tname), '.', quote_ident(cname), ' does not exist' ))));
+    END IF;
+    RETURN ok(
+        NOT _col_has_index_key(dbname, tname, cname),
+        concat('Column ', quote_ident(tname), '.', quote_ident(cname), ' should not have INDEX key' )
+    );
+END //
+
+
 
 /****************************************************************************/
 
