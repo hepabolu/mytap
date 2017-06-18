9fa51f05ee0f8ed9aceda8f43fa31f0ad95a5f63
Helma van der Linden <h.vanderlinden@omnihis.nl>
2017-06-18 19:20:47 +0200

    setting dev version to 0.07

--- mytap.sql
+++ mytap.sql
@@ -30,7 +30,7 @@ DROP FUNCTION IF EXISTS mytap_version //
 CREATE FUNCTION mytap_version()
 RETURNS VARCHAR(10)
 BEGIN
-    RETURN '0.06';
+    RETURN '0.07';
 END //
 
 DROP FUNCTION IF EXISTS mysql_version //
