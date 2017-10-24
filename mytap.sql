c81aac3653801c6d07092065ef743eb8af0ea230
Paul Campbell <paul@pjcampbell.net>
2017-10-24 11:55:46 +0100

    utility methods for quotes, datatypes, and multi-value checks

--- mytap.sql
+++ mytap.sql
@@ -509,17 +509,76 @@ END //
 DROP FUNCTION IF EXISTS quote_ident //
 CREATE FUNCTION quote_ident(ident TEXT) RETURNS TEXT
 BEGIN
+
     IF LOCATE('ANSI_QUOTES', @@SQL_MODE) > 0 THEN
         IF is_reserved(ident) OR locate('"', ident) > 0 THEN
             RETURN concat('"', replace(ident, '"', '""'), '"');
         END IF;
-    ELSE
+    ELSE 
         IF is_reserved(ident) OR locate('`', ident) > 0 THEN
             RETURN concat('`', replace(ident, '`', '``'), '`');
         END IF;
     END IF;
+
     RETURN ident;
 END //
+
+-- Just do it
+-- I wish people wouldn't choose identifiers that need quoting,
+-- but they do, and often just because they can. MySQL has no 
+-- array type (yet), which would make a lot of this simpler, so 
+-- we have to lay down some rules about how identifiers
+-- can be presented for tests. In the end this boils down to:
+
+-- lists must have all elements quoted 
+-- and 
+-- there can't be any spaces between elements because they are 
+-- legal characters in a quoted identifier. 
+
+-- In addition, all identifiers need be presented in a 
+-- consistent manner because the lack of function overloading means
+-- you can't have similarly named functions to deal with multi-valued
+-- identifier checks and scalar value ones. This, in turn, means
+-- that scalars that wouldn't normally need quoting need to be and
+-- that the quoting method needs to be consistent.
+ 
+DROP FUNCTION IF EXISTS qi //
+CREATE FUNCTION qi(ident TEXT) RETURNS TEXT
+BEGIN
+-- It's quoted already return it 
+-- replace ANSI quotes with backticks
+-- add backticks to everything else
+-- This won't work for people who use quotes within identifiers
+-- but they deserve everything coming to them anyway.
+
+  IF LEFT(ident,1) = '`' AND RIGHT(ident,1) = '`' THEN
+	RETURN ident;
+  END IF;
+   
+  IF LEFT(ident,1) = '"' AND RIGHT(ident,1) = '"' THEN
+	RETURN CONCAT('`', TRIM(BOTH '"' FROM ident) ,'`');
+  END IF;
+   
+  RETURN CONCAT('`', ident, '`');
+END //
+
+
+DROP FUNCTION IF EXISTS uqi //
+CREATE FUNCTION uqi(ident TEXT) RETURNS TEXT
+BEGIN
+-- We may want to unquote it for the sake of comparison
+
+  IF LEFT(ident,1) = '`' AND RIGHT(ident,1) = '`' THEN
+	RETURN TRIM(BOTH '`' FROM REPLACE(ident,'``','`'));
+  END IF;
+   
+  IF LEFT(ident,1) = '"' AND RIGHT(ident,1) = '"' THEN
+	RETURN TRIM(BOTH '"' FROM REPLACE(ident,'""','"'));
+  END IF;
+   
+  RETURN ident;
+END //
+
 
 -- check_test( test_output, pass, name, description, diag, match_diag )
 DROP FUNCTION IF EXISTS check_test //
@@ -659,6 +718,26 @@ BEGIN
     RETURN tap;
 END //
 
+-- fix up a comma separated list of values to compare 
+-- against a list of db objects
+DROP FUNCTION IF EXISTS _fixCSL //
+CREATE FUNCTION _fixCSL (want TEXT)  
+RETURNS TEXT
+BEGIN
+
+	SET want = REPLACE(want, '''','');
+	SET want = REPLACE(want, '"','');
+	SET want = REPLACE(want, '\n',''); 
+
+-- invalid characters eg NUL byte and characters > U+10000
+--   IF want REGEXP '[[.NUL.]\\u10000-\\u10FFFD]' = 1 THEN
+--		RETURN NULL;
+-- 	END IF;
+
+	RETURN want;
+END //
+
+
 -- general pgTAP method to pretty print differences in lists
 DROP FUNCTION IF EXISTS _are //
 CREATE FUNCTION _are (what TEXT, extras TEXT, missing TEXT, description TEXT)
@@ -667,31 +746,56 @@ BEGIN
 DECLARE msg TEXT    DEFAULT '';
 DECLARE res BOOLEAN DEFAULT TRUE;
 
-    IF extras <> '' THEN
-        SET res = FALSE;
-        SET msg = CONCAT('\n', CONCAT('\n'
-            '    Extra ', what, ':\n\t' , REPLACE( extras, ',', '\n\t')));
-    END IF;
+  IF extras <> '' THEN
+    SET res = FALSE;
+    SET msg = CONCAT('\n', CONCAT('\n'
+      '    Extra ', what, ':\n\t' , REPLACE( extras, ',', '\n\t')));
+  END IF;
     
-    IF missing <> '' THEN
-        SET res = FALSE;
-        SET msg = CONCAT(msg, CONCAT('\n'
-            '    Missing ', what, ':\n\t' , REPLACE( missing, ',', '\n\t')));
-    END IF;
+  IF missing <> '' THEN
+    SET res = FALSE;
+    SET msg = CONCAT(msg, CONCAT('\n'
+      '    Missing ', what, ':\n\t' , REPLACE( missing, ',', '\n\t')));
+  END IF;
 
-    RETURN CONCAT(ok(res, description), diag(msg));
+  RETURN CONCAT(ok(res, description), diag(msg));
+END //
+
+
+-- de-alias datatype synonyms to ensure consistent test for equality
+-- a test shouldn't fail because it uses a valid synonym
+-- sql_mode REAL_AS_FLOAT changes the aliased type when set
+
+DROP FUNCTION IF EXISTS _datatype //
+CREATE FUNCTION _datatype(word TEXT) RETURNS TEXT
+BEGIN
+
+  SET word =
+    CASE 
+      WHEN word IN ('BOOL', 'BOOLEAN') THEN 'TINYINT' 
+      WHEN word =  'INTEGER' THEN 'INT' 
+      WHEN word IN ('DEC', 'NUMERIC', 'FIXED') THEN 'DECIMAL' 
+      WHEN word IN ('DOUBLE_PRECISION') THEN 'DOUBLE'
+	  WHEN word = 'REAL' THEN IF (INSTR(@@GLOBAL.sql_mode, 'REAL_AS_FLOAT') > 0 , 'FLOAT' , 'DOUBLE')
+      WHEN word IN ('NCHAR', 'CHARACTER', 'NATIONAL_CHARACTER') THEN 'CHAR'
+      WHEN word IN ('NVARCHAR', 'VARCHARACTER', 'CHARACTER_VARYING', 'NATIONAL_VARCHAR') THEN 'VARCHAR'
+      WHEN word = 'CHAR_BYTE' THEN 'BIT'
+	END ;
+
+  RETURN word;
 END //
 
 
 DELIMITER ;
 
-source ./mytap-engine.sql
-source ./mytap-collation.sql
-source ./mytap-charset.sql
-source ./mytap-user.sql
-source ./mytap-table.sql
-source ./mytap-view.sql
-source ./mytap-column.sql
-source ./mytap-routines.sql
+source ./mytap-engine.sql;
+source ./mytap-collation.sql;
+source ./mytap-charset.sql;
+source ./mytap-user.sql;
+source ./mytap-table.sql;
+source ./mytap-view.sql;
+source ./mytap-column.sql;
+source ./mytap-routines.sql;
+source ./mytap-index.sql;
 
 SET GLOBAL log_bin_trust_function_creators = 0;
