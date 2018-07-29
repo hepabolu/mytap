19305e79aa9882554e931380f88661c5e0bb043c
Paul Campbell <paul@pjcampbell.net>
2018-07-29 19:42:48 +0100

    update tests in v8 for bug, feature changes and removal

--- tests/test-mytap-table.sql
+++ tests/test-mytap-table.sql
@@ -29,12 +29,12 @@ CREATE PROCEDURE taptest.createtable()
 DETERMINISTIC
 BEGIN
   -- This procedure allows create table syntax to accomodate changes in
-  -- in 5.7 for virtual columns
+  -- in 5.7.6 for virtual columns
   DECLARE myver INT;
 
   SET myver = (SELECT tap.mysql_version());
 
-  CASE WHEN myver > 507000 THEN -- virtual fields allowed in 5.7
+  CASE WHEN myver > 507006 THEN -- virtual fields allowed in 5.7.6
     SET @sql1 = '
       CREATE TABLE taptest.othertab(
       id      INT NOT NULL PRIMARY KEY,
@@ -45,7 +45,7 @@ BEGIN
       plain   INT,
       virt    INT AS (plain * 3) VIRTUAL
       ) ENGINE=INNODB, CHARACTER SET utf8, COLLATE utf8_general_ci';
-  WHEN myver > 506000 THEN -- fractional seconds stored in 5.6
+  WHEN myver > 506004 THEN -- fractional seconds stored in 5.6.4
     SET @sql1 = '
       CREATE TABLE taptest.othertab(
       id      INT NOT NULL PRIMARY KEY,
@@ -412,6 +412,7 @@ SELECT tap.check_test(
 
 -- NB
 -- 8.0.11 version adds columns.srs_id so this will have to change again
+-- but appears to have a bug - so temporarily disabled
 
 SELECT
    CASE WHEN tap.mysql_version() < 506004 THEN
@@ -421,7 +422,7 @@ SELECT
         'table_sha1() full specification',
         null,
         null,
-      0)
+        0)
    WHEN tap.mysql_version() < 507006 THEN
       tap.check_test(
         tap.table_sha1_is('taptest', 'othertab', '4a6803e5e0972b8dd96e05c59148187904678e7f', ''),
@@ -429,15 +430,17 @@ SELECT
         'table_sha1() full specification',
         null,
         null,
-      0)
-   WHEN tap.mysql_version() >= 507006 THEN
+        0)
+   WHEN tap.mysql_version() < 800011 THEN
       tap.check_test(
         tap.table_sha1_is('taptest', 'othertab', '9953062d687b36cfa4f1c83191708d55c7cfb976', ''),
         true,
         'table_sha1() full specification',
         null,
         null,
-      0)
+        0)
+   WHEN tap.mysql_version() >= 800011 THEN
+        tap.skip(1,'table_sha1_is() disabled due to MySQL bug in 8.0.11')
 END ;
 
 
@@ -449,7 +452,7 @@ SELECT
         'table_sha1() partial specification',
         null,
         null,
-      0)
+        0)
    WHEN tap.mysql_version() < 507006 THEN
       tap.check_test(
         tap.table_sha1_is('taptest', 'othertab', '4a6803e5', ''),
@@ -457,45 +460,59 @@ SELECT
         'table_sha1() partial specification',
         null,
         null,
-      0)
-   WHEN tap.mysql_version() >= 507006 THEN
+        0)
+  WHEN tap.mysql_version() < 800011 THEN
       tap.check_test(
         tap.table_sha1_is('taptest', 'othertab', '9953062d', ''),
         true,
         'table_sha1() partial specification',
         null,
         null,
-      0)
+        0)
+  WHEN tap.mysql_version() >= 800011 THEN
+    tap.skip(1,'table_sha1_is() disabled due to MySQL bug in 8.0.11')
 END ;
 
 
-SELECT tap.check_test(
-    tap.table_sha1_is('taptest', 'sometab', '0123456789',''),
-    false,
-    'table_sha1() incorrect specification',
-    null,
-    null,
-    0
-);
+SELECT
+  CASE WHEN tap.mysql_version() < 800011 THEN
+    tap.check_test(
+      tap.table_sha1_is('taptest', 'sometab', '0123456789',''),
+      false,
+      'table_sha1() incorrect specification',
+      null,
+      null,
+      0)
+  WHEN tap.mysql_version() >= 800011 THEN
+    tap.skip(1,'table_sha1_is() disabled due to MySQL bug in 8.0.11')
+END;
 
-SELECT tap.check_test(
-    tap.table_sha1_is('taptest', 'nonexistent', '1111111111',''),
-    false,
-    'table_sha1() nonexistent table',
-    null,
-    'Table taptest.nonexistent does not exist',
-    0
-);
+SELECT
+  CASE WHEN tap.mysql_version() < 800011 THEN
+    tap.check_test(
+      tap.table_sha1_is('taptest', 'nonexistent', '1111111111',''),
+      false,
+      'table_sha1() nonexistent table',
+      null,
+      'Table taptest.nonexistent does not exist',
+      0)
+   WHEN tap.mysql_version() >= 800011 THEN
+     tap.skip(2,'table_sha1_is() disabled due to MySQL bug in 8.0.11')
+END;
 
 
-SELECT tap.check_test(
-    tap.table_sha1_is('taptest', 'sometab', '1111111111', ''),
-    false,
-    'table_sha1() default description',
-    'Table taptest.sometab definition should match expected value',
-    null,
-    0
-);
+SELECT
+  CASE WHEN tap.mysql_version() < 800011 THEN
+    tap.check_test(
+      tap.table_sha1_is('taptest', 'sometab', '1111111111', ''),
+      false,
+      'table_sha1() default description',
+      'Table taptest.sometab definition should match expected value',
+      null,
+      0)
+   WHEN tap.mysql_version() >= 800011 THEN
+     tap.skip(2,'table_sha1_is() disabled due to MySQL bug in 8.0.11')
+END;
 
 
 /****************************************************************************/
