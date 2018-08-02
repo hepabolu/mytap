3ebe6b762983eb2d82bf0e124da50983b85197ee
Paul Campbell <paul@pjcampbell.net>
2018-08-02 15:02:52 +0100

    convenience functopm long form username

--- mytap-user.sql
+++ mytap-user.sql
@@ -50,6 +50,62 @@ END //
 
 
 /****************************************************************************/
+-- long-form user definition
+
+DROP FUNCTION IF EXISTS _has_user_at_host //
+CREATE FUNCTION _has_user_at_host(uname CHAR(97))
+RETURNS BOOLEAN
+DETERMINISTIC
+BEGIN
+  DECLARE ret BOOLEAN;
+
+  SELECT 1 INTO ret
+  FROM `mysql`.`user`
+  WHERE CONCAT(`user`, '@', `host`) = uname;
+
+  RETURN COALESCE(ret, 0);
+END //
+
+
+-- has_user@host(userdef, description )
+DROP FUNCTION IF EXISTS has_user_at_host //
+CREATE FUNCTION has_user_at_host(uname CHAR(97), description TEXT)
+RETURNS TEXT
+DETERMINISTIC
+BEGIN
+  -- normalize for different quoting or none
+  SET @uname = uname;
+  SET @uname = REPLACE(@uname,'`','');
+  SET @uname = REPLACE(@uname,'''','');
+
+  IF description = '' THEN
+    SET description = CONCAT('User ', uname, ' should exist');
+  END IF;
+
+  RETURN ok(_has_user_at_host(uname), description);
+END //
+
+
+-- hasnt_user_at_host(userdef, description)
+DROP FUNCTION IF EXISTS hasnt_user_at_host //
+CREATE FUNCTION hasnt_user_at_host(uname CHAR(97), description TEXT)
+RETURNS TEXT
+DETERMINISTIC
+BEGIN
+  -- normalize for different quoting or none
+  SET @uname = uname;
+  SET @uname = REPLACE(@uname,'`','');
+  SET @uname = REPLACE(@uname,'''','');
+
+  IF description = '' THEN
+    SET description = CONCAT('User ', uname, ' should not exist');
+  END IF;
+
+  RETURN ok(NOT _has_user_at_host(uname), description);
+END //
+
+
+/****************************************************************************/
 
 -- check user is not disabled 
 
