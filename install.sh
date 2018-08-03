83beef6808a8b7b646d939c34403d11b417f4c56
Paul Campbell <paul@pjcampbell.net>
2018-08-04 00:28:53 +0100

    add socket to installer

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 # Installation script for MyTAP
 
 
-SQLHOST=localhost;
+SQLHOST='localhost';
 SQLPORT=3306;
+SQLSOCK=''
 NOTESTS=0
 NOINSTALL=0
 FILTER=0
@@ -27,6 +28,10 @@ while [[ "$#" > 0 ]]; do
 	    SQLPORT="$2"
 	    shift
 	    ;;
+	-S|--socket)
+	    SQLSOCK="$2"
+	    shift
+	    ;;
 	-f|--filter)
 	    NOFILTER=0
 	    FILTER="$2"
@@ -44,13 +49,14 @@ Usage:
  install.sh [options]
 
 Options:
- -u, --user           MySQL username
- -p, --password       MySQL password
- -P, --port           MySQL port
- -h, --host           MySQL host
- -t, --no-tests       Don't run the test suite when the install is completed
- -i, --no-install     Don't perform the installation, i.e. just run the test suite
- -f, --filter         Perform the action on one class of objects <matching|eq|moretap|todo|utils|charset|collation|column|constraint|engine|event|index|partition|role|routines|table|trigger|schemata|user|view>
+ -u, --user string      MySQL username
+ -p, --password string  MySQL password
+ -h, --host name or IP  MySQL host
+ -P, --port name        MySQL port
+ -S, --socket filename  MySQL host
+ -t, --no-tests         Don't run the test suite when the install is completed
+ -i, --no-install       Don't perform the installation, i.e. just run the test suite
+ -f, --filter string    Perform the action on one class of objects <matching|eq|moretap|todo|utils|charset|collation|column|constraint|engine|event|index|partition|role|routines|table|trigger|schemata|user|view>
 EOF
 	   exit 1 
 	   ;;
@@ -66,17 +72,18 @@ MYSQLOPTS="--disable-pager --batch --raw --skip-column-names --unbuffered"
 
 if [[ $SQLUSER != '' ]] && [[ $SQLPASS != '' ]]; then
     MYSQLOPTS="$MYSQLOPTS -u$SQLUSER -p$SQLPASS";
-    CMD="--user $SQLUSER --password = $SQLPASS";
+fi
+
+if [[ $SQLSOCK != '' ]]; then
+   MYSQLOPTS="$MYSQLOPTS --socket=$SQLSOCK";
 fi
 
 if [[ $SQLHOST != 'localhost' ]]; then
-   MYSQLOPTS="$MYSQLOPTS --host $SQLHOST";
-   CMD="$CMD --host $SQLHOST";
+   MYSQLOPTS="$MYSQLOPTS --host=$SQLHOST";
 fi
 
 if [[ $SQLPORT != '3306' ]]; then
-  MYSQLOPTS="$MYSQLOPTS --port $SQLPORT"
-  CMD="$CMD --port $SQLPORT"
+  MYSQLOPTS="$MYSQLOPTS --port=$SQLPORT"
 fi
 
 MYVER1=`mysql $MYSQLOPTS --execute "SELECT @@global.version" | awk -F'-' '{print $1}' | awk -F'.' '{print $1 * 100000 }'`;
@@ -84,7 +91,7 @@ MYVER2=`mysql $MYSQLOPTS --execute "SELECT @@global.version" | awk -F'-' '{print
 MYVER3=`mysql $MYSQLOPTS --execute "SELECT @@global.version" | awk -F'-' '{print $1}' | awk -F'.' '{print $3}'`;
 
 
-MYVER=$(($MYVER1 + $MYVER2 + $MYVER3));
+MYVER=$(("$MYVER1" + "$MYVER2" + "$MYVER3"));
 
 # import the full package before running the tests
 # you can't use a wildcard with the source command so all version specific files need
