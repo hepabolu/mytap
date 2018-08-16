d25d9c7c5a47e521d0d03a6441b5f7c1126b41d2
Paul Campbell <paul@pjcampbell.net>
2018-08-16 21:37:40 +0100

    native auth

--- .travis.install-mysql.sh
+++ .travis.install-mysql.sh
@@ -8,9 +8,9 @@ set -v
 if [ ! -z "${DB}" ]; then
     # disable existing database server in case of accidential connection
     sudo service mysql stop
-
+command: --default-authentication-plugin=mysql_native_password
     docker pull ${DB}
-    docker run -it --name=mysqld -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes  DEFAULT_AUTHENTICATION_PLUGIN=mysql_native_password -p 3306:3306 ${DB}
+    docker run -it --name=mysqld -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -p 3306:3306 ${DB}
     sleep 10
 
     mysql() {
@@ -29,6 +29,8 @@ if [ ! -z "${DB}" ]; then
 
     mysql -e 'select VERSION()'
 
+    if [ $DB == 'mysql:8.0' ]; then
+	mysql -h 127.0.0.1 -P3306 -uroot -e "SET @@GLOBAL.default_authentication_plugin='mysql_native_password'"    
     fi
 else
     cat ~/.my.cnf
