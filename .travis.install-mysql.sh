5729c72d66920f9bc451ef5f26ae3be105e6cc57
Paul Campbell <paul@pjcampbell.net>
2018-08-16 18:41:57 +0100

    adapt from PyMySQL and use docker

--- .travis.install-mysql.sh
+++ .travis.install-mysql.sh
@@ -1,6 +1,48 @@
-echo mysql-apt-config mysql-apt-config/select-server select mysql-${MYVERSION} | sudo debconf-set-selections
-wget https://dev.mysql.com/get/mysql-apt-config_0.7.3-1_all.deb
-sudo dpkg --install mysql-apt-config_0.7.3-1_all.deb
-sudo apt-get update -q
-sudo apt-get install -q -y --allow-unauthenticated -o Dpkg::Options::=--force-confnew mysql-server
-sudo mysql_upgrade
+#!/bin/bash
+
+#debug
+set -x
+#verbose
+set -v
+
+if [ ! -z "${DB}" ]; then
+    # disable existing database server in case of accidential connection
+    sudo service mysql stop
+
+    docker pull ${DB}
+    docker run -it --name=mysqld -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -p 3306:3306 ${DB}
+    sleep 10
+
+    mysql() {
+        docker exec mysqld mysql "${@}"
+    }
+    while :
+    do
+        sleep 5
+        mysql -e 'select version()'
+        if [ $? = 0 ]; then
+            break
+        fi
+        echo "server logs"
+        docker logs --tail 5 mysqld
+    done
+
+    mysql -e 'select VERSION()'
+
+    if [ $DB == 'mysql:8.0' ]; then
+        WITH_PLUGIN='with mysql_native_password'
+        mysql -e 'SET GLOBAL local_infile=on'
+        docker cp mysqld:/var/lib/mysql/public_key.pem "${HOME}"
+        docker cp mysqld:/var/lib/mysql/ca.pem "${HOME}"
+        docker cp mysqld:/var/lib/mysql/server-cert.pem "${HOME}"
+        docker cp mysqld:/var/lib/mysql/client-key.pem "${HOME}"
+        docker cp mysqld:/var/lib/mysql/client-cert.pem "${HOME}"
+
+    else
+        WITH_PLUGIN=''
+    fi
+else
+    cat ~/.my.cnf
+
+    mysql -e 'select VERSION()'
+fi
