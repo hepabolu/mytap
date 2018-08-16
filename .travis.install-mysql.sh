024c70cac7a32acb79ff18fbe96c887d058a6e9a
Paul Campbell <paul@pjcampbell.net>
2018-08-16 20:24:47 +0100

    v8 login problem

--- .travis.install-mysql.sh
+++ .travis.install-mysql.sh
@@ -10,7 +10,7 @@ if [ ! -z "${DB}" ]; then
     sudo service mysql stop
 
     docker pull ${DB}
-    docker run -it --name=mysqld -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -p 3306:3306 ${DB}
+    docker run -it --name=mysqld -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes  DEFAULT_AUTHENTICATION_PLUGIN=mysql_native_password -p 3306:3306 ${DB}
     sleep 10
 
     mysql() {
@@ -29,17 +29,6 @@ if [ ! -z "${DB}" ]; then
 
     mysql -e 'select VERSION()'
 
-    if [ $DB == 'mysql:8.0' ]; then
-        WITH_PLUGIN='with mysql_native_password'
-        mysql -e 'SET GLOBAL local_infile=on'
-        docker cp mysqld:/var/lib/mysql/public_key.pem "${HOME}"
-        docker cp mysqld:/var/lib/mysql/ca.pem "${HOME}"
-        docker cp mysqld:/var/lib/mysql/server-cert.pem "${HOME}"
-        docker cp mysqld:/var/lib/mysql/client-key.pem "${HOME}"
-        docker cp mysqld:/var/lib/mysql/client-cert.pem "${HOME}"
-
-    else
-        WITH_PLUGIN=''
     fi
 else
     cat ~/.my.cnf
