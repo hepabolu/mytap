b900af40a171ee316f0f3123029374b46115e5eb
Paul Campbell <paul@pjcampbell.net>
2018-08-17 14:18:51 +0100

    get auth methods

--- .travis.install-mysql.sh
+++ .travis.install-mysql.sh
@@ -8,13 +8,13 @@ set -v
 if [ ! -z "${DB}" ]; then
     # disable existing database server in case of accidential connection
     sudo service mysql stop
-command: --default-authentication-plugin=mysql_native_password
+
     docker pull ${DB}
     docker run -it --name=mysqld -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -p 3306:3306 ${DB}
     sleep 10
 
     mysql() {
-        docker exec mysqld mysql --default-authentication-plugin=mysql_native_password "${@}"
+        docker exec mysqld mysql "${@}"
     }
     while :
     do
@@ -28,9 +28,10 @@ command: --default-authentication-plugin=mysql_native_password
     done
 
     mysql -e 'select VERSION()'
-
+    mysql -e 'SELECT user, host, plugin FROM mysql.user'
 else
     cat ~/.my.cnf
 
     mysql -e 'select VERSION()'
+    mysql -e 'SELECT user, host, plugin FROM mysql.user'
 fi
