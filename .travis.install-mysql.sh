8354fde8e645925dfd39ae66e4c2fd9180a043f3
Paul Campbell <paul@pjcampbell.net>
2018-08-17 22:37:51 +0100

    nearly

--- .travis.install-mysql.sh
+++ .travis.install-mysql.sh
@@ -36,14 +36,8 @@ if [ ! -z "${DB}" ]; then
 	docker cp mysqld:/var/lib/mysql/client-key.pem "${HOME}"
 	docker cp mysqld:/var/lib/mysql/client-cert.pem "${HOME}"
     fi
-    # The following are all included in an attempt to get v8.0 access to work
-    # by using mysql_native_password, it still refuses to work with
-    # ERROR 2059 (HY000): Authentication plugin 'caching_sha2_password' cannot be loaded: /usr/lib/mysql/plugin/caching_sha2_password.so:
-    # cannot open shared object file: No such file or directory
-    # No matter they don't hinder operation for any other version
-    # so I'm leaving them in
-    mysql -u root -e "UPDATE mysql.user SET plugin = 'mysql_native_password'; FLUSH PRIVILEGES;"
-    mysql -u root -e "CREATE USER 'mytap'@'%' IDENTIFIED WITH mysql_native_password; GRANT ALL on *.* TO 'mytap'@'%';"
+
+    mysql -u root -e "CREATE USER 'mytap'@'%' IDENTIFIED WITH mysql_native_password; GRANT ALL on *.* TO 'mytap'@'%'; FLUSH PRIVILEGES"
     mysql -e 'SELECT user, host, plugin FROM mysql.user'
 else
     cat ~/.my.cnf
