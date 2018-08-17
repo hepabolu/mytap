992a555de8df17526b79849047aebfe2b0bdd95d
Paul Campbell <paul@pjcampbell.net>
2018-08-18 00:20:23 +0100

    oh so close now

--- .travis.install-mysql.sh
+++ .travis.install-mysql.sh
@@ -13,29 +13,21 @@ if [ ! -z "${DB}" ]; then
     docker run -it --name=mysqld -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -p 3306:3306 ${DB}
     sleep 10
 
-    mysql() {
-        docker exec mysqld mysql "${@}"
-    }
-    while :
-    do
-        sleep 5
-        mysql -e 'select version()'
-        if [ $? = 0 ]; then
-            break
-        fi
-        echo "server logs"
-        docker logs --tail 5 mysqld
-    done
+#    mysql() {
+#        docker exec mysqld mysql "${@}"
+#    }
+#    while :
+#    do
+#        sleep 5
+#        mysql -e 'select version()'
+#        if [ $? = 0 ]; then
+#            break
+#        fi
+#        echo "server logs"
+#        docker logs --tail 5 mysqld
+#    done
 
     mysql -e 'select VERSION()'
-
-#   if [ $DB == 'mysql:8.0' ]; then
-#	docker cp mysqld:/var/lib/mysql/public_key.pem "${HOME}"
-#	docker cp mysqld:/var/lib/mysql/ca.pem "${HOME}"
-#	docker cp mysqld:/var/lib/mysql/server-cert.pem "${HOME}"
-#	docker cp mysqld:/var/lib/mysql/client-key.pem "${HOME}"
-#	docker cp mysqld:/var/lib/mysql/client-cert.pem "${HOME}"
-#    fi
 
     if [ $DB == 'mysql:8.0' ]; then
       mysql -e 'SELECT user, host, plugin, authentication_string, password_expired, password_lifetime, account_locked FROM mysql.user'
