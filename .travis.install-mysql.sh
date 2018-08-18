d934f257a939deda64e86ea4dc431ab70177ac66
Paul Campbell <paul@pjcampbell.net>
2018-08-18 13:06:57 +0100

    needed for local access

--- .travis.install-mysql.sh
+++ .travis.install-mysql.sh
@@ -13,26 +13,26 @@ if [ ! -z "${DB}" ]; then
     docker run -it --name=mysqld -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -p 3306:3306 ${DB}
     sleep 10
 
-#    mysql() {
-#        docker exec mysqld mysql "${@}"
-#    }
-#    while :
-#    do
-#        sleep 5
-#        mysql -e 'select version()'
-#        if [ $? = 0 ]; then
-#            break
-#        fi
-#        echo "server logs"
-#        docker logs --tail 5 mysqld
-#    done
+    mysql() {
+        docker exec mysqld mysql "${@}"
+    }
+    while :
+    do
+        sleep 5
+        mysql -e 'select version()'
+        if [ $? = 0 ]; then
+            break
+        fi
+        echo "server logs"
+        docker logs --tail 5 mysqld
+    done
 
     mysql -e 'select VERSION()'
 
     if [ $DB == 'mysql:8.0' ]; then
       mysql -e 'SELECT user, host, plugin, authentication_string, password_expired, password_lifetime, account_locked FROM mysql.user'
-      mysql -u root -e "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user != 'mysql.infoschema'";
-      mysql -u root -e "CREATE USER 'mytap'@'%' IDENTIFIED WITH mysql_native_password; GRANT ALL on *.* TO 'mytap'@'%'; FLUSH PRIVILEGES;"
+      mysql -e "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user != 'mysql.infoschema'";
+      mysql -e "CREATE USER 'mytap'@'%' IDENTIFIED WITH mysql_native_password; GRANT ALL on *.* TO 'mytap'@'%'; FLUSH PRIVILEGES;"
       mysql -e 'SELECT user, host, plugin, authentication_string, password_expired, password_lifetime, account_locked FROM mysql.user'
     fi
 else
